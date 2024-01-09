896ec83d4afe12f18a812722e3fdab693c5c8f40
ssorguc <sorgucselma@gmail.com>
2024-01-09 19:57:19 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+let movieNightSnacks = [
+    "Popcorn",
+    "Nachos with Cheese",
+    "Chocolate-covered Pretzels",
+    "Caramel Popcorn",
+    "Sour Gummy Worms",
+    "Pizza Rolls",
+    "Soft Pretzels",
+    "M&Ms",
+    "Cheese and Crackers",
+    "Ice Cream",
+    "Cotton Candy",
+    "Chips and Salsa",
+    "Chicken Wings",
+    "Fruit Skewers",
+    "Brownie Bites"
+];
